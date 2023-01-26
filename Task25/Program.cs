25c8abe82895c46621afca368f064706fe2f5857
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-26 23:30:58 +0300

    add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите число A: ");
+int A = int.Parse(ReadLine());
+Write("Введите число B: ");
+int B = int.Parse(ReadLine());
+double exponentiation = GetNumberExponentiation(A, B);
+WriteLine($" {A} в степени {B} = {exponentiation}");
+
+
+
+
+double GetNumberExponentiation(int number, int degree)
+{
+    double exp = Math.Pow(number, degree);
+    return exp;
+}
