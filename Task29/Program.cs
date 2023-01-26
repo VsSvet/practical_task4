ac4eec0bab63ad54a04239d63ea1153bbac6ef15
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-27 00:35:04 +0300

    add Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите 8 цифр через пробел: ");
+string[] Array = ReadLine().Split(" ");
+WriteLine("[{0}]", string.Join(", ", Array));
