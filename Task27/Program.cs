3256fd8820787a736a054e3d9f8c533d42e64645
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-26 23:35:44 +0300

    add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите число N: ");
+int N = int.Parse(ReadLine());
+int summa = GetSumNumbers(N);
+WriteLine($"Сумма = {summa}");
+
+
+
+
+int GetSumNumbers(int number)
+{
+    int sum = 0;
+    for(int i = 1; i<=number; i++)
+    {
+        sum += number%10;
+        number = number/10;
+    }
+    return sum;
+}
